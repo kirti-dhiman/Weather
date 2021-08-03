647197afe7ae5d91618277b9ef693f0a1a47fe5a
Kirti Dhiman <kirti.dhiman@nagarro.com>
2021-08-03 12:22:27 +0530

    Formatted the file

--- app/repositories/WeatherInterface.php
+++ app/repositories/WeatherInterface.php
@@ -1,8 +1,7 @@
 <?php namespace App\Repositories;
 
 interface WeatherInterface
-{
-    
+{    
     public function addWeatherRecord(array $data);
     public function fetchCachedRecords($city, $date);
 }
